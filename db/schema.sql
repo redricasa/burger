e703a92668de53e97cc5c2e2d1e17361bd7bfe8c
redricasa <fredeb26@gmail.com>
2019-10-30 23:11:53 -0700

    connected to jaws db and ran a query with the database name given.

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
-DROP DATABASE IF EXISTS burger_db;
-CREATE DATABASE burger_db;
-USE burger_db;
+-- the database name is jawsDB given db name that I have to use in order for the heroku app to connect to mysql workbench
+DROP DATABASE IF EXISTS xi39uusv5rxkzw9r;
+CREATE DATABASE xi39uusv5rxkzw9r;
+USE xi39uusv5rxkzw9r;
 
 CREATE TABLE burgers (
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOL DEFAULT false,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
-
 
