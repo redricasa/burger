53b355c9b5823d01ab879cd8c1531a23ad225b55
redricasa <fredeb26@gmail.com>
2019-11-04 18:27:34 -0800

    in order to use .env to store passwords, one has to require dotenv. config. and module.export it to be used in the orm.js file

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,9 @@
 //connect to mysql with root password in keys.js
-var dbpass = process.env.dbpass;
+require("dotenv").config();
+var dbpass = process.env.DBPASS;
 var mysql = require('mysql');
-// -----------------------------------------
-//add jawsdb as a connection- added the JAWSDB_URL in .env
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
-//creating a function to place in the else statement
-// var connect =  {
-//     local:mysql.createConnection(
-//         {
-//             host: "localhost",
-//             port: 3306,
-//             database:"burger_db",
-//             user:"root",
-//             password: dbpass
-//         }
-//     )
-// };
-//connect to heroku's jawsdb database or mysql locally
-// if (process.env.JAWSDB_URL) {
-//     connection; 
-// } 
-// else {
-//     connect ;
-// };
-// connect.connect(function(err){
-//     if (err) throw err;
-//     console.log("Connected to mySql!!")
-// });
-// module.exports = connect;
-// ------------------------------------------
-var connection;
+
+var connection ;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL); 
     console.log("connected to Heroku's Jawsdb!")
@@ -43,3 +17,4 @@ if (process.env.JAWSDB_URL) {
     });
     console.log("connected to local mysql workbench!")
 };
+module.exports = connection;
